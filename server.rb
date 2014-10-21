d7f463a3ad124ac0aa566aaa2c3c3ee124a3a8f0
terut <terut.dev+github@gmail.com>
2014-10-21 16:18:59 +0900

    Add logger

--- server.rb
+++ server.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 
 module Koyabu
   class Server < Sinatra::Base
+    configure do
+      enable :logging
+      file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+      file.sync = true
+      use Rack::CommonLogger, file
+    end
+
     register Sinatra::Gyazo
 
     get '/' do
